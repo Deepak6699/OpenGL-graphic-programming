532647841a7f1f6af6bdf6f3fd9203476f61b8ef
Deepak <deepak.rockstar1231@gmail.com>
2021-03-06 10:26:19 +0530

    Textures in OpenGl

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -4,16 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <sstream>	
 
 #include "Renderer.h"
 
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
-
+#include "VertexBufferLayout.h"
 #include "Shader.h"
-
+#include "Texture.h"
 
 
 int main(void)
@@ -49,10 +49,10 @@ int main(void)
 	{
 		float positions[] =
 		{
-			-0.5f, -0.5f,//0
-			 0.5f, -0.5f,//1
-			 0.5f, 0.5f,//2
-			-0.5f, 0.5f,//3
+			-0.5f, -0.5f, 0.0f, 0.0f,//0
+			 0.5f, -0.5f, 1.0f, 0.0f,//1
+			 0.5f, 0.5f, 1.0f, 1.0f,//2
+			-0.5f, 0.5f, 0.0f, 1.0f,//3
 		};
 
 		unsigned int indices[] =
@@ -61,14 +61,14 @@ int main(void)
 			2,3,0,
 		};
 
-		unsigned int vao;
-		GLCall(glGenVertexArrays(1, &vao));
-		GLCall(glBindVertexArray(vao));
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb,layout);
 		 
@@ -79,27 +79,32 @@ int main(void)
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
-		float r = 0.0f;
-		float increment = 0.05f;
+		
+		Texture texture("res/textures/texture.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture",0);
 
 		va.Unbind();
-		shader.Unbind();
 		vb.Unbind();
 		ib.Unbind();
+		shader.Unbind();
+
+		Renderer renderer;
+
+		float r = 0.0f;
+		float increment = 0.05f;
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
-			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+			renderer.Clear();
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-			va.Bind();
-			ib.Bind();
-
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+			renderer.Draw(va, ib, shader);
 
 			if (r > 1.0f)
 				increment = -0.05f;
